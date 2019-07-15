48363aed890c8b3c58719da52ee902aad33fe47e
Clayton Hickey <clayton.hickey@yahoo.com>
2019-07-15 05:44:03 -0400

    Added custom port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express'),
     path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 var modules = {
     defaultHead: function () {
